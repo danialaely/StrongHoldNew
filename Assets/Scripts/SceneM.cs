f6649b52ab41a1cc24a211ed70351c8ec56dca3f
Danial Siddiqi <66087554+danialaely@users.noreply.github.com>
2024-07-19 23:59:24 +0500

    Multiplayer room

--- Assets/Scripts/SceneM.cs
+++ Assets/Scripts/SceneM.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using Photon.Realtime;
+using UnityEngine.UI;
 
 public class SceneM : MonoBehaviourPunCallbacks
 {
@@ -13,8 +15,30 @@ public class SceneM : MonoBehaviourPunCallbacks
     public GameObject LobbyPanel;
     public GameObject ConnectingPanel;
     public GameObject CreateRoomPanel;
+    public GameObject RoomListPanel;
 
     public TMP_InputField roomNameText;
+
+    private Dictionary<string, RoomInfo> roomListData;
+
+    public GameObject roomListPrefab;
+    public GameObject roomListParent;
+
+    private Dictionary<string, GameObject> roomListGameobject;
+
+    [Header("Inside Room Panel")]
+    public GameObject InsideRoomPanel;
+
+    private void Start()
+    {
+        roomListData = new Dictionary<string, RoomInfo>();
+        roomListGameobject = new Dictionary<string, GameObject>();
+    }
+    private void Update()
+    {
+        Debug.Log("Network state:"+PhotonNetwork.NetworkClientState);
+       // Debug.Log("Room List Prefab:"+roomListPrefab+" "+" RoomListParent:"+roomListParent);
+    }
 
     public void LoadPlayScene()
     {
@@ -35,15 +59,36 @@ public class SceneM : MonoBehaviourPunCallbacks
     public void OnClickRoomCreate() 
     {
         string roomName = roomNameText.text;
-        if (string.IsNullOrEmpty(roomName)) 
+        if (!string.IsNullOrEmpty(roomName)) 
         {
             roomName = roomName+Random.Range(0, 1000);
         }
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = (byte)2;
+        PhotonNetwork.CreateRoom(roomName,roomOptions);
     }
 
-    private void Update()
+    public void OnCancelClick() 
     {
-        Debug.Log("Network state:"+PhotonNetwork.NetworkClientState);
+        ActivateMyPanel(LobbyPanel.name);
+    }
+
+    public void RoomListBtnClicked() 
+    {
+        if (!PhotonNetwork.InLobby) 
+        {
+            PhotonNetwork.JoinLobby();
+        }
+        ActivateMyPanel(RoomListPanel.name);
+    }
+
+    public void BackFromRoomList() 
+    {
+        if (PhotonNetwork.InLobby) 
+        {
+            PhotonNetwork.LeaveLobby();
+        }
+        ActivateMyPanel(LobbyPanel.name);
     }
 
     public void ActivateMyPanel(string panelName)
@@ -51,7 +96,32 @@ public class SceneM : MonoBehaviourPunCallbacks
         LobbyPanel.SetActive(panelName.Equals(LobbyPanel.name));
         ConnectingPanel.SetActive(panelName.Equals(ConnectingPanel.name));
         CreateRoomPanel.SetActive(panelName.Equals(CreateRoomPanel.name));
+        RoomListPanel.SetActive(panelName.Equals(RoomListPanel.name));
+        InsideRoomPanel.SetActive(panelName.Equals(InsideRoomPanel.name));
     }
+
+    public void RoomJoinFromList(string roomName) 
+    {
+        if (PhotonNetwork.InLobby) 
+        {
+            PhotonNetwork.LeaveLobby();
+            PhotonNetwork.JoinRoom(roomName);
+        }
+    }
+
+    public void ClearRoomList() 
+    {
+        if (roomListGameobject.Count > 0) 
+        {
+            foreach (var v in roomListGameobject.Values) 
+            {
+                Destroy(v);
+            }
+            roomListGameobject.Clear();
+        }
+    }
+
+
 
     #region Photon_Callbacks
     public override void OnConnected()
@@ -63,6 +133,68 @@ public class SceneM : MonoBehaviourPunCallbacks
     {
         Debug.Log(PhotonNetwork.LocalPlayer.NickName+" is connected to photon.");
         ActivateMyPanel(LobbyPanel.name);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log(PhotonNetwork.CurrentRoom.Name+" is created");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName+" joined the room");
+        ActivateMyPanel(InsideRoomPanel.name);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        //Clear List
+        ClearRoomList();
+        Debug.Log("OnRoomList");
+
+        foreach(RoomInfo rooms in roomList) 
+        {
+            Debug.Log("Room Name:"+rooms.Name);
+            if (!rooms.IsOpen || !rooms.IsVisible || rooms.RemovedFromList)
+            {
+                if (roomListData.ContainsKey(rooms.Name))
+                {
+                    roomListData.Remove(rooms.Name);
+                }
+            }
+            else 
+            {
+                if (roomListData.ContainsKey(rooms.Name))
+                {
+                    //Update List
+                    roomListData[rooms.Name] = rooms;
+                }
+                else 
+                {
+                    roomListData.Add(rooms.Name,rooms);
+                }
+            }
+        }
+
+        //Generate List Item
+        foreach (RoomInfo roomItem in roomListData.Values) 
+        {
+            GameObject roomListItemObject = Instantiate(roomListPrefab);
+            roomListItemObject.transform.SetParent(roomListParent.transform);
+            roomListItemObject.transform.localScale = Vector3.one;
+
+            //room name  player Number  Button join room
+            roomListItemObject.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = roomItem.Name;
+            roomListItemObject.transform.GetChild(1).gameObject.GetComponent<TMP_Text>().text = roomItem.PlayerCount+"/"+roomItem.MaxPlayers;
+            roomListItemObject.transform.GetChild(2).gameObject.GetComponent<Button>().onClick.AddListener(()=> RoomJoinFromList(roomItem.Name));
+            roomListGameobject.Add(roomItem.Name, roomListItemObject);
+        }
+    }
+
+    public override void OnLeftLobby()
+    {
+        ClearRoomList();
+        roomListData.Clear();
     }
 
     #endregion
